f17ad5c87ca68a43d7e9a6aabad542d5b3fdf518
MaryamHoushyari <mhshyr96@gmail.com>
2022-10-16 17:49:20 +0330

    update method overriden in PostSerializer

--- post/serializers.py
+++ post/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from post.models import Post, PostScore
@@ -17,6 +18,29 @@ class PostSerializer(serializers.ModelSerializer):
         if not user.is_anonymous:
             scored = obj.scores.filter(author=user)
             return scored.first().score if scored else None
+
+    def update(self, instance, validated_data):
+        """
+        I've written this method because you asked me to consider this as real production
+
+        I think in real projects we may need to be able to create or update scores for a post in
+        the same url as we can update the post!
+        """
+        with transaction.atomic():
+            instance = super(PostSerializer, self).update(instance, validated_data)
+
+            if "score" in self.initial_data:
+                score = self.initial_data.get("score")
+                score["post"] = instance.id
+                old_score = PostScore.objects.filter(author=score["author"], post=score["post"])
+                if old_score:
+                    score_serializer = PostScoreSerializer(instance=old_score[0], data=score)
+                else:
+                    score_serializer = PostScoreSerializer(data=score)
+                score_serializer.is_valid(raise_exception=True)
+                score_serializer.save()
+
+            return instance
 
 
 class PostScoreSerializer(serializers.ModelSerializer):
